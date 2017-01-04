c962087b172c06e4367f20b878380161a27e2e81
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-05 01:04:41 +0700

    Small Bug Fix

--- minify/index__.php
+++ minify/index__.php
@@ -3,17 +3,17 @@
 Hook::set('shield.output', function($content) {
     $state = Extend::state(__DIR__);
     // Minify embedded CSS
-    if (strpos($content, '</style>') !== false) {
-        $content = preg_replace_callback('#<style(\s[^<>]*?)?([\s\S]*?)<\/style>#i', function($m) use($state) {
+    if (stripos($content, '</style>') !== false) {
+        $content = preg_replace_callback('#<style(\s[^<>]*?)?>([\s\S]*?)<\/style>#i', function($m) use($state) {
             array_unshift($state['css'], $m[2]);
-            return '<style' . $m[1] . call_user_func_array('Minify::css', $state['css']) . '</style>';
+            return '<style' . $m[1] . '>' . call_user_func_array('Minify::css', $state['css']) . '</style>';
         }, $content);
     }
     // Minify embedded JS
-    if (strpos($content, '</script>') !== false) {
-        $content = preg_replace_callback('#<script(\s[^<>]*?)?([\s\S]*?)<\/script>#i', function($m) use($state) {
+    if (stripos($content, '</script>') !== false) {
+        $content = preg_replace_callback('#<script(\s[^<>]*?)?>([\s\S]*?)<\/script>#i', function($m) use($state) {
             array_unshift($state['js'], $m[2]);
-            return '<script' . $m[1] . call_user_func_array('Minify::js', $state['js']) . '</script>';
+            return '<script' . $m[1] . '>' . call_user_func_array('Minify::js', $state['js']) . '</script>';
         }, $content);
     }
     // Minify HTML
