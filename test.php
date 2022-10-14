3e29031fb72a8d55cfe3b526babfe7534ebc923e
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-14 11:22:29 +0700

    Update

--- test.php
+++ test.php
@@ -11,8 +11,8 @@ foreach ([
         continue;
     }
     echo '<h2>' . $k . '</h2>';
-    echo '<pre style="border:3px solid #900;padding:3px;white-space:pre-wrap;word-wrap:break-word;"><code>' . htmlspecialchars($content) . '</code></pre>';
-    echo '<pre style="border:3px solid #090;padding:3px;white-space:pre-wrap;word-wrap:break-word;"><code>' . htmlspecialchars(call_user_func('Minify::' . $k, $content)) . '</code></pre>';
+    echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars($content) . '</pre>';
+    echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars(call_user_func('Minify::' . $k, $content)) . '</pre>';
 }
 
 exit;
