d86dbbd1a5e0b7c4a8e2784f8a6e3fedae0aba46
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-16 07:02:00 +0700

    Update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php namespace x;
 
-if (\defined("\\TEST") && 'x.minify' === \TEST) {
-    require __DIR__ . \D . 'test.php';
-}
-
 function minify($content) {
     return \Minify::HTML($content);
 }
 
 \Hook::set('content', __NAMESPACE__ . "\\minify", 2);
+
+if (\defined("\\TEST") && 'x.minify' === \TEST && \is_file($test = __DIR__ . \D . 'test.php')) {
+    require $test;
+}
