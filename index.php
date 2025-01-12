f62116ae15f4ec515a52fa00f8fdadb293fd6643
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-01-12 23:22:04 +0700

    Update

--- index.php
+++ index.php
@@ -8,7 +8,74 @@ require __DIR__ . \D . 'index' . \D . 'p-h-p.php';
 require __DIR__ . \D . 'index' . \D . 'x-m-l.php';
 
 function content($content) {
-    return \Minify::HTML($content);
+    if ($type = \type()) {
+        if ('text/css' === $type) {
+            return \Minify::CSS($content);
+        }
+        if ('text/html' === $type) {
+            return \Minify::HTML($content);
+        }
+        if (
+            'application/javascript' === $type ||
+            'application/x-javascript' === $type ||
+            'text/javascript' === $type
+        ) {
+            return \Minify::JS($content);
+        }
+        if (
+            'application/feed+json' === $type ||
+            'application/geo+json' === $type ||
+            'application/json' === $type ||
+            'application/ld+json' === $type ||
+            'text/json' === $type
+        ) {
+            return \Minify::JSON($content);
+        }
+        if (
+            'application/atom+xml' === $type ||
+            'application/mathml+xml' === $type ||
+            'application/rdf+xml' === $type ||
+            'application/rss+xml' === $type ||
+            'image/svg+xml' === $type ||
+            'text/xml' === $type
+        ) {
+            return \Minify::XML($content);
+        }
+    }
+    if ($x = \pathinfo(\lot('url')->path ?? "", \PATHINFO_EXTENSION)) {
+        if ('css' === $x) {
+            return \Minify::CSS($content);
+        }
+        if (
+            'htm' === $x ||
+            'html' === $x
+        ) {
+            return \Minify::HTML($content);
+        }
+        if (
+            'js' === $x ||
+            'mjs' === $x
+        ) {
+            return \Minify::JS($content);
+        }
+        if (
+            'json' === $x ||
+            'jsonp' === $x ||
+            'webmanifest' === $x
+        ) {
+            return \Minify::JSON($content);
+        }
+        if (
+            'svg' === $x ||
+            'xht' === $x ||
+            'xhtm' === $x ||
+            'xhtml' === $x ||
+            'xml' === $x
+        ) {
+            return \Minify::XML($content);
+        }
+    }
+    return $content;
 }
 
 \Hook::set('content', __NAMESPACE__ . "\\content", 2);
